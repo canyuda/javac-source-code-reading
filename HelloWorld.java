eb2cbd60f3a95d63a37799ede454be2da5e73842
canyuda <canyuda@changingedu.com>
2020-05-22 19:00:45 +0800

    删除点东西

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * CreateUser: canyuda
+ * CreateTime: 2020/5/22 18:02
+ * Description:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        switch (args[0]) {
+            case "1":
+                System.out.println("Hello World");
+                break;
+            case "2":
+                System.out.println("Hello World2");
+                break;
+            default:
+                System.out.println("Hello World3");
+                break;
+        }
+        return;
+    }
+}
